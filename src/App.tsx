1d7d8f1289fb3e6f8496ff974dc71e0995b5c047
Dimas Nurcahyo Putra <dimsdeall@gmail.com>
2023-01-19 13:57:30 +0700

    feat: new background

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { LoadingCowardly } from "./components/atoms";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col justify-center items-center bg-background">
-      <LoadingCowardly />
-      <p className="App-link">Website is under construction</p>
-      {/* <Button color=''>Test</Button> */}
+    <div className="flex min-h-screen flex-col justify-center items-center background-layer">
+      <div className="flex justify-center flex-col items-center bg-background p-3 border border-1 border-yellow rounded-lg opacity-90">
+        <LoadingCowardly />
+        <p className="App-link">Website is under construction</p>
+      </div>
     </div>
   );
 }
-
 export default App;
